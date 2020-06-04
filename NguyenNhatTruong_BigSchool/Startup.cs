8d06cc489e9bfc5d86b8acabc3322e9737f28c97
Administrator <Administrator@PC268>
2020-06-04 13:49:35 +0700

    Add project files.

---
+++ NguyenNhatTruong_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NguyenNhatTruong_BigSchool.Startup))]
+namespace NguyenNhatTruong_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
