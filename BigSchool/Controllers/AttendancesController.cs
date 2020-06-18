d6a7f2fe146413ef97c9e1eaad711e3788a1b033
SPCable <44009901+SPCable@users.noreply.github.com>
2020-06-18 15:44:09 +0700

    18/06/2020

--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BigSchool.DTOs;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,26 @@ namespace BigSchool.Controllers
 {
     public class AttendancesController : ApiController
     {
+        private ApplicationDbContext _dbContext;
+        public AttendancesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        [HttpPost]
+        public IHttpActionResult Atetend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+            if (_dbContext.Attendances.Any(a => a.AttendeeId == userId && a.CourseId == attendanceDto.CourseId))
+                return BadRequest("The Attendance already exists!");
+            var attendance = new Attendance
+            {
+                CourseId = attendanceDto.CourseId,
+                AttendeeId = userId
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+            return Ok();
+        }
     }
 }
--- BigSchool/Controllers/CoursesController.cs
+++ BigSchool/Controllers/CoursesController.cs
@@ -29,15 +29,15 @@ namespace BigSchool.Controllers
             return View(viewModel);
         }
 
-        [Authorize]
+
         [HttpPost]
         public ActionResult Create(CourseViewModel viewModel)
         {
-            if(!ModelState.IsValid)
-            {
-                viewModel.Categories = _dbContext.Categories.ToList();
-                return View("Create", viewModel);
-            }
+            //if(!ModelState.IsValid)
+            //{
+            //    viewModel.Categories = _dbContext.Categories.ToList();
+            //    return View("Create", viewModel);
+            //}
             var course = new Course
             {
                 LecturerId = User.Identity.GetUserId(),
